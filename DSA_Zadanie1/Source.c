63b1b5df97b696a04c838850f2664413ef15d2dc
Dominik Mikuška <dominik.mikuska@gmail.com>
2019-09-30 17:26:00 +0100

    Zaklad mallocu

--- DSA_Zadanie1/Source.c
+++ DSA_Zadanie1/Source.c
@@ -1,41 +1,62 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-/*
-typedef struct Pages {
-	int size;
-	struct Page *next;
-
-} Page;
-*/
-
-void *start;
+int* start; 
+//mozeme pouzivat char nie len void
 
 void memory_init(void *ptr, unsigned int size)
 {
-	start = ptr;
-	/**((short*)start) = (short)size;
-	printf("%p\n", start);
-	printf("%p\n", ((short*)start));
-	printf("%d\n", *((short*)start));*/
+	start = (int*)ptr;
+
+	*start = size;	//Zapisanie velkosti do prvy 4 bytes
+	
+	*(start + sizeof(int)) = 3*sizeof(int);	//Smernik na prvu volnu velkost
+	
+	*(start + 2*sizeof(int)) = size-2*sizeof(int);	//Zapisanie velkosti prvej volnej pamati
+	
+	*(start + 3*sizeof(int)) = -1;	//Smernik na druhu volnu velkost
+
 }
 
+void* FindPlace(int num, int size)
+{	
+	if (*(start + num - sizeof(int)) >= size)
+	{ //Ak miesto vyhovuje
+		printf("CISLO: %d\n", *(start + num-4));
+		return (void*)(start + num);
+	}
+	else //Ak je miesto moc male
+	{
+		return FindPlace(num + *(start + num - sizeof(int)), size);
+	}
+
+	//Pokial nenajde volne miesto
+	if (*(start + num) == -1)
+		return NULL;
+
+
+}
+
+void *memory_alloc(unsigned int size)
+{
+
+	return FindPlace(*(start+sizeof(int)), size);
+
+}
 
 int main()
 {
-
+	int num = 515;
 	char a[50];
 
 	memory_init(a, 50);
 
-	int size = 20;
-	int test = 1<<31;
-	int *p = &size;
+	int *test = (char*)memory_alloc(20);
+	test -= sizeof(int);
 
-	int lul = *p & ~0x1;
+	printf("NAJDENY ALLOC: %d\n",*test);
 
-	printf("%d\n", sizeof(int*));
-	//printf("%d\n", sizeof(test));
-	printf("%d", ((21+1)>>1)<<1);
+	//printf("%d", ((num+1)>>1)<<1);
 
 	getchar();
 	getchar();
