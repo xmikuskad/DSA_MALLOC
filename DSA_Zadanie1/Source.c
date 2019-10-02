b03f212886654fa14ff98d2bef71e9213d30fb9a
Dominik Mikuška <dominik.mikuska@gmail.com>
2019-10-02 16:53:29 +0100

    Malloc by mohol fungovat

--- DSA_Zadanie1/Source.c
+++ DSA_Zadanie1/Source.c
@@ -15,46 +15,142 @@ void memory_init(void *ptr, unsigned int size)
 	*(start + 2*sizeof(int)) = size-2*sizeof(int);	//Zapisanie velkosti prvej volnej pamati
 	
 	*(start + 3*sizeof(int)) = -1;	//Smernik na druhu volnu velkost
+	//POUZIVANIE -1 NAMIESTO NULL
 
+	*(start + 4 * sizeof(int)) = sizeof(int);	//Smernik na predchadzajucu volnu pamat
+	//zatial nepotrebne
 }
 
-void* FindPlace(int num, int size)
-{	
-	if (*(start + num - sizeof(int)) >= size)
-	{ //Ak miesto vyhovuje
-		printf("CISLO: %d\n", *(start + num-4));
-		return (void*)(start + num);
-	}
-	else //Ak je miesto moc male
-	{
-		return FindPlace(num + *(start + num - sizeof(int)), size);
-	}
-
-	//Pokial nenajde volne miesto
-	if (*(start + num) == -1)
-		return NULL;
-
-
-}
 
 void *memory_alloc(unsigned int size)
 {
+	if (size < 8) size = 8; //Zarovnanie najmenej na 8 bytes;
 
-	return FindPlace(*(start+sizeof(int)), size);
+	//Ak nemame ziadny volny blok - return
+	if(*(start + sizeof(int)) == -1)
+		return NULL;
 
+	int num = *(start + sizeof(int));
+	int tmp = sizeof(int); //ukazovatel na posledny odkaz NEXT
+
+
+	do
+	{
+		if (*(start + num - sizeof(int)) >= size && *(start + num - sizeof(int)) > 0)
+		{ //Ak miesto vyhovuje
+			//TODO
+			
+			//Vytvorenie novej particie ak zostalo viac ako 12bytes (4B hlavicka, 8B data) a nepresahujeme danu pamat
+			if (*(start + num - sizeof(int)) - size >= 12 && (num + size + sizeof(int)) <=50)
+			{
+				//Nastavenie velkosti noveho bloku
+				*(start + num + size) = *(start + num - sizeof(int)) - (size + sizeof(int));
+
+				//Zmensenie aktualnej velkosti
+				*(start + num - sizeof(int)) = size + sizeof(int);
+
+				//Zapisanie NEXT
+				*(start + num + size + sizeof(int)) = *(start + num);
+				//Zapisanie BEFORE
+				*(start + num + size + 2 * sizeof(int)) = tmp;
+
+				//Prepisanie NEXT minuleho bloku
+				*(start + tmp) = num + size + sizeof(int);
+
+			}
+			else
+			{
+  				*(start + tmp) = *(start + num); //Prepisanie NEXT na dalsi volny blok;
+				if (tmp != sizeof(int)) //Pokial nie sme na zaciatku (tam nie je BEFORE)
+				{
+					*(start + num + sizeof(int)) = *(start + tmp + sizeof(int));
+				}
+			}
+
+			//Nastavenie bitu na 1 - OBSADENY
+			printf("Old value %d\n", *(start + num - sizeof(int)));
+			*(start + num - sizeof(int)) |= 1 << 31;
+			printf("New value %d\n", *(start + num - sizeof(int)));
+
+
+			return (void*)(start + num);
+		}
+		else
+		{
+			tmp = num;
+			num += *(start + num - sizeof(int));
+		}
+	}
+	while (*(start + num) != -1);
+
+	return NULL;
+
+}
+
+void testik(char* p)
+{
+	printf("Vzdialenost %d\n", p-(char*)start);
 }
 
 int main()
 {
-	int num = 515;
 	char a[50];
+	char* test, test2, test3,test4;
 
 	memory_init(a, 50);
 
-	int *test = (char*)memory_alloc(20);
-	test -= sizeof(int);
+	test = (char*)memory_alloc(8);
 
-	printf("NAJDENY ALLOC: %d\n",*test);
+	if (test != NULL)
+	{
+		printf("Alokacia test1 uspesna\n\n");
+		//testik(test);
+	}
+	else
+	{
+		printf("Alokacia test1 neuspesna\n\n");
+	}
+	//test -= sizeof(int);
+
+	/*int pokus = 1;
+	printf("Pokus %d\n", pokus << 30);
+	printf("Pokus %d\n", pokus << 31);*/
+
+	//printf("NAJDENY ALLOC: %d\n",*test);
+	//testik(test);
+
+	test2 = (char*)memory_alloc(8);
+	if (test2 != NULL)
+	{
+		printf("Alokacia test2 uspesna\n\n");
+		//testik(test2);
+	}
+	else
+	{
+		printf("Alokacia test2 neuspesna\n\n");
+	}
+
+	test3 = (char*)memory_alloc(8);
+	if (test3 != NULL)
+	{
+		printf("Alokacia test3 uspesna\n\n");
+		//testik(test3);
+	}
+	else
+	{
+		printf("Alokacia test3 neuspesna\n\n");
+	}
+
+	test4 = (char*)memory_alloc(8);
+	if (test4 != NULL)
+	{
+		printf("Alokacia test4 uspesna\n\n");
+		//testik(test3);
+	}
+	else
+	{
+		printf("Alokacia test4 neuspesna\n\n");
+	}
 
 	//printf("%d", ((num+1)>>1)<<1);
 
